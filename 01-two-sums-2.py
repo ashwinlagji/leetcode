737c6f38214bdf308a3a4fbc8f6059176d00ab71
ashwinlagji <ashwinlagji@gmail.com>
2018-11-12 22:01:43 +0530

    problem 01 complete

---
+++ 01-two-sums-2.py
@@ -0,0 +1,18 @@
+# optimized code 
+# Runtime: 44 ms, faster than 65.97% of Python3 online submissions for Two Sum.
+# try 1
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        h = {}
+        for i, num in enumerate(nums):
+            n = target - num
+            if n not in h:
+                h[num] = i
+            else:
+                return [h[n], i]
