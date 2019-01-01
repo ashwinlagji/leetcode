2752f5659cacf720610c35bfc033c932141e419b
ashwinlagji <ashwinlagji@gmail.com>
2019-01-01 19:29:34 +0530

    Integer to roman solution 2

---
+++ 12_Integer_to_Roman_01.py
@@ -0,0 +1,39 @@
+# Optimized
+#
+
+
+class Solution(object):
+    def intToRoman(self, num):
+        """
+        :type num: int
+        :rtype: str
+        """
+        if num == 0:    return ""
+        elif num >= 1000:
+            return "M" + self.intToRoman(num-1000)
+        elif num >= 900:
+            return "CM" + self.intToRoman(num-900)
+        elif num >= 500:
+            return "D" + self.intToRoman(num-500)
+        elif num >= 400:
+            return "CD" + self.intToRoman(num-400)
+        elif num >= 100:
+            return "C" + self.intToRoman(num-100)
+        elif num >= 90:
+            return "XC" + self.intToRoman(num-90)
+        
+        elif num >= 50:
+            return "L" + self.intToRoman(num-50)
+        elif num >= 40:
+            return "XL" + self.intToRoman(num-40)
+        
+        elif num >= 10:
+            return "X" + self.intToRoman(num-10)
+        elif num == 9:
+            return "IX"
+        elif num >= 5:
+            return "V" + self.intToRoman(num-5)
+        elif num == 4:
+            return "IV"
+        else:
+            return "I"*num
