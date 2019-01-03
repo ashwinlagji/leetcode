9a21a894b4a983f748dcbfaf62ddd91cb4e3228b
ashwinlagji <ashwinlagji@gmail.com>
2019-01-03 22:53:49 +0530

    longest prefix optimized solution

---
+++ 14_Longest_Common_Prefix_01.py
@@ -0,0 +1,17 @@
+# second menthod
+# Runtime: 32 ms, faster than 38.89% of Python online submissions for Longest Common Prefix.
+
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        if not strs:
+            return ""
+        shortest = min(strs,key=len)
+        for i, ch in enumerate(shortest):
+            for other in strs:
+                if other[i] != ch:
+                    return shortest[:i]
+        return shortest 
