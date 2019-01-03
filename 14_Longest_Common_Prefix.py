8cf5bbb32ce8f0e400840e968560e2e82c380556
ashwinlagji <ashwinlagji@gmail.com>
2019-01-03 22:53:33 +0530

    longest prefix solution 1

---
+++ 14_Longest_Common_Prefix.py
@@ -0,0 +1,23 @@
+# Optimised
+
+# Runtime: 40 ms, faster than 19.29% of Python online submissions for Longest Common Prefix.
+
+class Solution:
+
+    def longestCommonPrefix(self, strs):
+        """
+        Time:  O(pn)  [where p = len(the longest prefix), n = len(strs)]
+        Space: O(1)
+        """
+        if not strs:
+            return ""
+
+        minlen = min((len(string) for string in strs))
+        prefix_length = -1
+        for i in range(minlen):
+            current_char = strs[0][i]
+            if not all(string[i] == current_char for string in strs):
+                break
+            else:
+                prefix_length = i
+        return "" if prefix_length == -1 else strs[0][: prefix_length + 1]
